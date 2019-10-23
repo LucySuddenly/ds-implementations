909a3fff4c19a43950c6fd07d9da971565ab37d2
Lucy Suddenly <suddenlylucy@gmail.com>
2019-10-23 15:33:36 -0700

    basic BST node tests

---
+++ bst/node.test.js
@@ -0,0 +1,12 @@
+const Node = require("./node.js")
+
+test('new Node should return a Node object', () => {
+    expect(new Node("test").constructor).toBe(Node)
+})
+
+test('new Node should be initialized with data', () => {
+    const node = new Node("test")
+    expect(node.data).toBeTruthy()
+    expect(node.data).toBe("test")
+})
+
