bd544fc0994055d1f3556cbf4693aa08daca06a6
Lucy Suddenly <suddenlylucy@gmail.com>
2019-10-24 14:41:46 -0700

    testing  BST insert method

--- bst/bst.test.js
+++ bst/bst.test.js
@@ -13,3 +13,17 @@ test("if a BST's root is null, .insert creates a root node whose data is equal t
     bst.insert("test")
     expect(bst.root.data).toBe("test")
 })
+
+describe("if a BST's root data !null,", () => {
+    let bst = new BST()
+    bst.insert(1)
+    bst.insert(0)
+    bst.insert(2)
+    test(".insert adds a node to the left pointer if its argument is less than the root's data", () => {
+        expect(bst.root.left.data).toBe(0)
+    })
+    test(".insert adds a node to the right pointer if its argument is less than the root's data", () => {
+        expect(bst.root.right.data).toBe(2)
+    })
+})
+
