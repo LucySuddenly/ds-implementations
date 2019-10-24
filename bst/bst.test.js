a31da6a2caa87844f0bdc9902ab759c04c901ebd
Lucy Suddenly <suddenlylucy@gmail.com>
2019-10-24 10:11:14 -0700

    BST initialization testing

--- bst/bst.test.js
+++ bst/bst.test.js
@@ -3,3 +3,7 @@ const BST = require("./bst.js")
 test("new BST should return a BST object",() => {
     expect(new BST().constructor).toBe(BST)
 })
+
+test("new BST should be initialized without a root node", () => {
+    expect(new BST().root).toBe(null)
+})
