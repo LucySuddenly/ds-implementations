dc233f6bc247a60b98ae8f92dbcb36b42bcfad7b
Lucy Suddenly <suddenlylucy@gmail.com>
2019-10-24 10:18:39 -0700

    test wording change

--- bst/bst.test.js
+++ bst/bst.test.js
@@ -4,6 +4,6 @@ test("new BST should return a BST object",() => {
     expect(new BST().constructor).toBe(BST)
 })
 
-test("new BST should be initialized without a root node", () => {
+test("new BST should be initialized with a null a root node", () => {
     expect(new BST().root).toBe(null)
 })
