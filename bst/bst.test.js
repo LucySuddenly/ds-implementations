70a30e5ca745545d027a1015f3c416cb7e793942
Lucy Suddenly <suddenlylucy@gmail.com>
2019-10-24 13:32:01 -0700

    BST insert testing

--- bst/bst.test.js
+++ bst/bst.test.js
@@ -7,3 +7,9 @@ test("new BST should return a BST object",() => {
 test("new BST should be initialized with a null a root node", () => {
     expect(new BST().root).toBe(null)
 })
+
+test("if a BST's root is null, .insert creates a root node whose data is equal to .insert's argument", () => {
+    let bst = new BST()
+    bst.insert("test")
+    expect(bst.root.data).toBe("test")
+})
