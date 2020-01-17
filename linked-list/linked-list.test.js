40d1cfb488de18a9ff7c722c72aae080c09a49b4
Lucy Suddenly <suddenlylucy@gmail.com>
2020-01-17 13:12:42 -0800

    refactored and added to linked list test

--- linked-list/linked-list.test.js
+++ linked-list/linked-list.test.js
@@ -1,9 +1,16 @@
 const LinkedList = require('./linked-list')
 
+let ll = new LinkedList()
+
 test("new LinkedList should return a LinkedList object",() => {
-    expect(new LinkedList().constructor).toBe(LinkedList)
+    expect(ll.constructor).toBe(LinkedList)
 })
 
 test("new LinkedList should be initialized with a null a root node", () => {
-    expect(new LinkedList().root).toBe(null)
+    expect(ll.root).toBe(null)
+})
+
+test("if LinkedList root is null, .insert() sets the root node", () => {
+    ll.insert(5)
+    expect(ll.root.data).toBe(5)
 })
